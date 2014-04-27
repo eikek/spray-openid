093a6871a2c8fed9313a843e9961042ddc259263
Eike Kettner <eike.kettner@gmail.com>
2014-04-28 00:15:21 +0200

    set next version 0.2.0-SNAPSHOT

--- project/SprayOpenId.scala
+++ project/SprayOpenId.scala
@@ -56,7 +56,7 @@ object SprayOpenId extends sbt.Build {
   }
   
   override lazy val settings = super.settings ++ Seq(
-    version := "0.1.0",
+    version := "0.2.0-SNAPSHOT",
     resolvers ++= Seq("spray repo" at "http://repo.spray.io"),
     publishTo := Some("eknet-maven2" at "https://eknet.org/maven2"),
     credentials += Credentials(Path.userHome / ".ivy2" / ".credentials"),
